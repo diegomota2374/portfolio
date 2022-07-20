eb9d5ed74a2405151eb66222720fc1c3f5f3da19
diegomota2374 <diegomota2374@gmail.com>
2022-07-20 15:56:08 -0300

    customizado o backgrauld

--- portfolio-01/src/contexts/ColorModeContext.tsx
+++ portfolio-01/src/contexts/ColorModeContext.tsx
@@ -18,15 +18,83 @@ export const ColorModeProvider = ({children}: ColorModeProps) => {
       }),
       [],
     );
+    const lightColor = '#AAAAAA'
+    const darkColor = '#212121'
+    const mainColor = mode === 'light' ? lightColor : darkColor
+
+    const lightColorPaper = '#AAAAAA'
+    const darkColorPaper = '#212121'
+    const mainColorPaper = mode === 'light' ? lightColorPaper : darkColorPaper
   
     const theme = useMemo(
       () =>
         createTheme({
           palette: {
             mode,
+            common: {
+              black: '#000',
+              white: '#FFF'
+            },
+            primary: {
+              light: '#9E69FD',
+              main: '#9155FD',
+              dark: '#804BDF',
+              contrastText: '#FFF'
+            },
+            secondary: {
+              light: '#9C9FA4',
+              main: '#8A8D93',
+              dark: '#777B82',
+              contrastText: '#FFF'
+            },
+            success: {
+              light: '#6AD01F',
+              main: '#56CA00',
+              dark: '#4CB200',
+              contrastText: '#FFF'
+            },
+            error: {
+              light: '#FF6166',
+              main: '#FF4C51',
+              dark: '#E04347',
+              contrastText: '#FFF'
+            },
+            warning: {
+              light: '#FFCA64',
+              main: '#FFB400',
+              dark: '#E09E00',
+              contrastText: '#FFF'
+            },
+            info: {
+              light: '#32BAFF',
+              main: '#16B1FF',
+              dark: '#139CE0',
+              contrastText: '#FFF'
+            },
+            grey: {
+              50: '#FAFAFA',
+              100: '#F5F5F5',
+              200: '#EEEEEE',
+              300: '#E0E0E0',
+              400: '#BDBDBD',
+              500: '#9E9E9E',
+              600: '#757575',
+              700: '#616161',
+              800: '#424242',
+              900: '#212121',
+              A100: '#D5D5D5',
+              A200: '#AAAAAA',
+              A400: '#616161',
+              A700: '#303030'
+            },
+            background:{
+              default:mainColor,
+              paper:mainColorPaper,
+            }
           },
+          
         }),
-      [mode],
+      [mode, mainColor, mainColorPaper],
     );
   
     return (
